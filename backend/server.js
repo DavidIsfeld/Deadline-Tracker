781cd712a27d9d732cc768304dafe2a8ea03318f
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2022-09-24 13:49:41 -0400

    Set up server to listen to a certain port after connecting to the mongodb database.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@
 // this .env file is ignored in this repository, so you must create your own if you wish to use this program.
 require('dotenv').config();
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 
 // express app
 const app = express();
@@ -11,3 +13,17 @@ const app = express();
 // middleware
 app.use(express.json());
 
+// routes
+app.use('/api/user', userRoutes);
+
+// connect to the database before the server starts listening to a certain port
+mongoose.connect(process.env.MONG_URI)
+.then(() => {
+    // listen to requests from a certain port
+    app.listen(process.env.PORT, () => {
+        console.log('Server listening');
+    });
+})
+.catch((err) => {
+    console.log(err);
+});
