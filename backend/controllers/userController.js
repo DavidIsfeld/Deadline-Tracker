d83b7ca324541e9ec11e851a71a2821499c0c3c5
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2022-09-24 14:53:48 -0400

    implemented creating new users in the database and returning a json web token after they sign up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,15 @@
 // this file contains functions that deal with requests related to user activities (logging in etc)
 
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// function to create a json web token
+// the end of the token is "signed" with a string of characters generated using a secret string known only to the server
+// this string is stored in the .env file of this project
+// we create the token using the user's id in the database, as this is not sensitive information
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -9,7 +18,24 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-    console.log('signup user');
+    // request will be sent in json format thanks to the express.json() middleware in server.js
+    // get the email and password used from the request
+    const { email, password } = req.body;
+
+    try {
+        // signup the user
+        const user = await User.signup(email, password);
+
+        // json web tokens are used to verify that the user is logged in, after the user sign up we log them in, so we will send them back a token now
+        // create a token using the user's id given to them by mongodb
+        const token = createToken(user._id);
+
+        // send a response back to the user containing the token and the user's email
+        res.status(200).json({email, token});
+    } catch (error) {
+        // send a response back to the user containing the error if something goes wrong
+        res.status(400).json({error: error.message})
+    }
 };
 
 module.exports = { loginUser, signupUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,8 @@
 // this file contains the model of a user stored in a mongodb database using mongoose.
 
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -16,5 +18,38 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+// static signup method to signup users
+userSchema.statics.signup = async function(email, password) {
+    // check that an email and password were sent with the request
+    if (!email || !password) {
+        throw Error('An email and password is needed to create an account');
+    }
+
+    // check that a real email was sent in the request
+    if (!validator.isEmail(email)) {
+        throw Error('The entered email is not a valid email');
+    }
+
+    // check if the email has already been used
+    const emailUsed = await this.findOne({ email });
+
+    if (emailUsed) {
+        throw Error('This email is already in use');
+    }
+
+    // Now that we know that the email is valid and can be used, we can store this user in the database,
+    // thus creating a new user.
+    // We will hash the password before storing it using bcrypt, in case the database is ever breached
+
+    // add salt to the end of the password
+    const salt = await bcrypt.genSalt(10);
+
+    const hash = await bcrypt.hash(password, salt);
+
+    // create a user in the database and return information about said user
+    const user = this.create({ email, password: hash });
+    return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
